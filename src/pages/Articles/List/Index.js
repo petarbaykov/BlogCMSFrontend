e45beb5420984959c53a53e261e2f3eb9d29f15b
Petar Baykov <petbaik26@gmail.com>
2023-01-20 22:33:46 +0200

    improve manage articles design

--- src/pages/Articles/List/Index.js
+++ src/pages/Articles/List/Index.js
@@ -21,28 +21,58 @@ export default function ArticlesList() {
 
     return (
         <div className="container mx-auto">
-            <Link to="/posts/create">Add new Article</Link>
-            <table className="table-auto">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                { articles.map(article => (
+
+            <Link
+                to="/posts/create"
+                className="text-white inline-block my-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+                Add new Article
+            </Link>
+            <br />
+            <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <td>{ article.title }</td>
-                        <td>
-                            <Link to={`/posts/edit/${article.id}`}>Edit</Link>
-                        </td>
-                        <td>
-                            <button onClick={() => deleteArticle(article.id)}>Delete</button>
-                        </td>
+                        <th scope="col" className="px-6 py-3">
+                            Title
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Category
+                        </th>
+                        <th scope="col" colSpan="2" className="px-6 py-3">
+                            Actions
+                        </th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        { articles.map(article => (
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <td
+                                    scope="row"
+                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
+                                    { article.title }
+                                </td>
+                                <td className="px-6 py-4">{ article.category.name }</td>
+                                <td className="px-6 py-4" colSpan="2">
+                                    <Link
+                                        to={`/posts/edit/${article.id}`}
+                                        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                                    >
+                                        Edit
+                                    </Link>
+                                    <button
+                                        onClick={() => deleteArticle(article.id)}
+                                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
 
     );
